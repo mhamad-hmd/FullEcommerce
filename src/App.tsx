aba5e4b1d29dbdd6db4b2443037e1b15210a9c00
mhamad-hmd <107796257+mhamad-hmd@users.noreply.github.com>
2022-09-11 10:55:48 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+     <div className='text-3xl text-gray'>Works</div>
+    </div>
+  )
+}
+
+export default App
