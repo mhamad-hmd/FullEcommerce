caee6f8f7854b71c2da1cdaa6a4369b0b7f404c9
mhamad <mhamad.hmd20@gmail.com>
2022-09-29 21:13:33 +0300

    pc

--- src/requestMethods.ts
+++ src/requestMethods.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 
 const BASE_URL = "https://full-ecommerce-api.herokuapp.com/api"; 
 
-const TOKEN  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMjJkYjliMzRkYWIzYWRmY2Q4OWJjNyIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NjM1NzM4NDksImV4cCI6MTY2MzgzMzA0OX0.AF88ZLAw5UqUiVkFeiTO0n9z_wtCA29LwFag9COYNnw    "
+const user = JSON.parse(localStorage.getItem("persist:root") || '{}')?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN  =  currentUser?.accessToken
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
