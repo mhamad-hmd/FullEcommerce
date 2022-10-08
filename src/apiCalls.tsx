3d18d9bfe14f135042fd498eb66ea3a3b342ba31
mhamad-hmd <107796257+mhamad-hmd@users.noreply.github.com>
2022-10-08 08:10:46 +0300

    lptp

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -10,7 +10,7 @@ export const login = async (user: Object) => {
         useUserStore.setState({
             logging: {
                 logginStart: true,
-                loginSuccess: false,
+                loginSuccess: true,
                 register:false
             }
         })
